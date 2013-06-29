0bbe27f2cb45c9b9050915e55ff3b488b58d0d71
Cory <corydiamand@corydiamand-K55N.(none)>
2013-06-28 21:12:58 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Coryslist::Application.config.secret_token = '4e7bdedc44492f84982e1b1b478f4260ebc02dfa4df93453ce8a90072081b80dbc90d261ec6cc923d1acf46a9da21d97c75d85cba85b23309bd9e5a5a8964092'
