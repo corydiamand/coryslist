2b6e12db47ed4791f25e1dabc1f8ce31f024cd2e
Cory Diamand <cory@postpourri.com>
2013-06-30 14:19:34 -0400

    good enough to use

--- app/controllers/task_controller.rb
+++ app/controllers/task_controller.rb
@@ -7,6 +7,7 @@ class TaskController < ApplicationController
 
 	def create
 		@task = Task.new(params[:task])
+		@task.status = false
 		@task.save
 		redirect_to dashboard_path
 	end
@@ -17,6 +18,19 @@ class TaskController < ApplicationController
 	def destroy
 		@task = Task.find(params[:id])
 		@task.destroy
+		redirect_to dashboard_path
+	end
+
+	def update
+		@task = Task.find(params[:id])
+		if @task.status == nil
+			@task.status = true
+		elsif @task.status == true
+			@task.status = false
+		elsif @task.status == false
+			@task.status = true
+		end
+		@task.save
 		redirect_to dashboard_path
 	end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130629174346) do
+ActiveRecord::Schema.define(:version => 20130630164350) do
 
   create_table "lists", :force => true do |t|
     t.string   "title"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(:version => 20130629174346) do
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
     t.integer  "list_id"
+    t.boolean  "status"
   end
 
   create_table "users", :force => true do |t|
